e6087cbace6b9666b2845e26af019a95e4a1e641
RupakBoral <boralrupak@gmail.com>
2026-01-18 19:40:43 +0530

    zip

--- Basics/zip.py
+++
@@ -1,9 +0,0 @@
-# zip function is used to combine multiple iterable
-
-names = ["Harry", "Boston", "Rick", "Sarah"]
-ages =  [21, 20, 17, 23]
-
-for name, age in zip(names, ages):
-    print(f"Name: {name}, Ages: {age}")
-
-
--- Basics/zip1.py
+++ Basics/zip1.py
@@ -1,3 +1,12 @@
+# zip function is used to combine multiple iterable
+
+names = ["Harry", "Boston", "Rick", "Sarah"]
+ages =  [21, 20, 17, 23]
+
+for name, age in zip(names, ages):
+    print(f"Name: {name}, Ages: {age}")
+
+
 food = ["cornflakes", "briyani", "roti & panner"] 
 time = ("breakfast", "lunch", "dinner")
 
