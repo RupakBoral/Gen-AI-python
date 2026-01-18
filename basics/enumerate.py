6fb4597fc5238e5b26a9b335b6218548c03c7b7d
Rupak Boral <126939617+RupakBoral@users.noreply.github.com>
2026-01-18 19:44:13 +0530

    Delete basics directory

--- basics/enumerate.py
+++
@@ -1,4 +0,0 @@
-fruits = ['apple', 'banana', 'mango', 'cherry']
-
-for idx, fruit in enumerate(fruits, start=101):
-    print(f"{idx}: {fruit}")
--- basics/walrus.py
+++
@@ -1,5 +0,0 @@
-value = 10
-num = int(input("Enter a divisor"))
-
-if remainder := value % num:
-    print(f"Not divisible by {num}, remainder is: {remainder}")
